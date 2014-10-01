2d5d09bf31209b5bb8d8c0dbc664c5498d51ed71
xurtasun <xavi.urta@gmail.com>
2014-10-01 13:10:49 +0200

    f commit

---
+++ src/main/java/edu/upc/eetac/dsa/xurtasun/echoSOckets/Client.java
@@ -0,0 +1,33 @@
+package edu.upc.eetac.dsa.xurtasun.echoSOckets;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        
+
+        String hostName = "10.89.52.206";
+        int portNumber = 2001;
+
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader( new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }
+}
